fd9d3feb67a01eaad0879a91b76a657135cd171d
Ambrose <ambrosedyx@163.com>
2023-06-12 21:44:48 +0800

    0612

--- 链表/removeFromListNode.go
+++ 链表/removeFromListNode.go
@@ -2,6 +2,23 @@ package main
 
 func removeZeroSumSublists(head *ListNode) *ListNode {
 	dummy := &ListNode{Val:0}
-	du
-
+	dummy.Next = head;
+	seen := map[int]*ListNode{}
+	prefix := 0
+	for node:=dummy; node != nil; node = node.Next {
+		prefix += node.Val
+		seen[prefix] = node
+	}
+	prefix = 0
+	for node:= dummy; node != nil; node = node.Next {
+		prefix += node.Val
+		node.Next = seen[prefix].Next
+	}
+	return dummy.Next
 }
+
+1,2,-3,3,1
+
+{0:-3,1:1,3:3,4:1}
+
+0 3 
