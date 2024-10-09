75954bf08cf1c5604334c87f2eff21df23690615
Ambrose <ambrosedyx@163.com>
2024-10-09 23:29:00 +0800

    [feat][一百道动态规划]121股票交易

--- 100dynamic/121.py
+++ 100dynamic/121.py
@@ -11,3 +11,17 @@ class Solution:
             dp[i][1] = prices[i] + dp[i-1][0]
             dp[i][2] = max(dp[i-1][1], dp[i-1][2])
         return dp[n-1][1] if dp[n-1][1] > dp[n-1][2] else dp[n-1][2]
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        minprice = max(prices) + 1
+        res = 0
+        for i in range(n):
+            if (prices[i] < minprice):
+                minprice = prices[i]
+            else:
+                res = res if res > prices[i] - minprice else prices[i] - minprice
+        return res
+            
